9587a529cc6e3a90a7ceafe4300610e1b61e7f58
Murilo Cristiano <muricristiano@gmail.com>
2023-07-08 18:49:51 +0200

    first components created, structured project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { Post } from "./components/Post";
+import { Header } from './components/Header/Header.jsx';
+import { Sidebar } from './components/Sidebar/Sidebar.jsx';
+import { Post } from "./components/Post/Post";
+
+import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
-      <h1>Hello world</h1>
-      <Post 
-        author="Murilo Cristiano" 
-        content="Post de exemplo Post de exemplo Post de exemplo Post de exemplo Post de exemplo Post de exemplo Post de exemplo "/>
+      
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+          <main>
+            <Post 
+              author="Murilo Cristiano" 
+              content="Post de exemplo Post de exemplo Post de exemplo Post de exemplo Post de exemplo Post de exemplo Post de exemplo "
+            />
+          </main>
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+
+import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css';
+const img1 = 'https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+        <img 
+            className={styles.cover} 
+            src={img1} 
+            alt=''
+        />
+
+        <div className={styles.profile}>
+            <img 
+                className={styles.avatar} 
+                src='https://avatars.githubusercontent.com/u/98929176?v=4'
+            />
+
+            <strong>Murilo Cristiano</strong>
+            <span>JavaScript Developer</span>
+        </div>
+
+        <footer>
+            <a href="#">
+                <PencilLine size={15} weight='light' />
+                Editar seu perfil
+            </a>
+        </footer>
+    </aside>
+  );
+}
